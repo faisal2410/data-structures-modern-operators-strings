e13651fbf912f883644f1865fbd22f14f5c0cbc3
faisal2410 <faisal2410@yahoo.com>
2022-01-29 11:53:35 +0600

    script.js added

---
+++ script.js
@@ -0,0 +1,9 @@
+// Destructuring Arrays
+'use strict';
+const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto']
+}
